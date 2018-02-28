746210ecf0a7e1eca908c5b59c1fbe15ab0d80be
abdo16khaled <abdullah16khaled@gmail.com>
2018-02-28 23:28:32 +0200

    Feat: wala 7aga

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MENU_ITEMS } from '../dashboard/dashboard-menu';
 import { NbMenuItem } from '@nebular/theme/components/menu/menu.service';
+import { LocalDataSource } from 'ng2-smart-table';
+
 
 @Component({
   selector: 'app-items',
@@ -13,5 +15,76 @@ export class ItemsComponent implements OnInit {
 
   ngOnInit() {
     this.menu = MENU_ITEMS;
+  }
+
+}
+
+@Component({
+  selector: 'SmartTableComponent',
+  templateUrl: './items.component.html',
+  styles: [`
+    nb-card {
+      transform: translate3d(0, 0, 0);
+    }
+  `],
+})
+ class SmartTableComponent  implements OnInit{
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      firstName: {
+        title: 'First Name',
+        type: 'string',
+      },
+      lastName: {
+        title: 'Last Name',
+        type: 'string',
+      },
+      username: {
+        title: 'Username',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      age: {
+        title: 'Age',
+        type: 'number',
+      },
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private service: SmartTableService) {
+    const data = this.service.getData();
+    this.source.load(data);
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
   }
 }
--- src/app/items/items.module.ts
+++ src/app/items/items.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { ItemsRoutingModule } from './items-routing.module';
-
 import { ItemsComponent } from './items.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 @NgModule({
-  imports: [ThemeModule, ItemsRoutingModule],
+  imports: [ThemeModule, ItemsRoutingModule, Ng2SmartTableModule],
   declarations: [ItemsComponent],
   entryComponents: [],
   providers: []
